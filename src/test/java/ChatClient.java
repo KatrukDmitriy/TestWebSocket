f5bdafae22d5c26ec7f0465e205ca8b5a2e23ae9
Dmytro_Katruk <dmitriykatrukk@gmail.com>
2021-07-06 11:48:31 +0300

    test

--- src/test/java/ChatClient.java
+++ src/test/java/ChatClient.java
@@ -1,146 +1,67 @@
-import java.awt.Container;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.net.URI;
-import java.net.URISyntaxException;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
 import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.drafts.Draft;
-import org.java_websocket.drafts.Draft_6455;
 import org.java_websocket.handshake.ServerHandshake;
 
-public class ChatClient extends JFrame implements ActionListener {
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class ChatClient {
+
+    private static final String locationToConnect = System.getenv("location");
+    private static final String remote = System.getenv("remoteDriver");
+
+    private static final String remote1 = System.getenv("51541541lmkfgnlsjrn");
 
     private static final long serialVersionUID = -6056260699202978657L;
 
-    private final JTextField uriField;
-    private final JButton connect;
-    private final JButton close;
-    private final JTextArea ta;
-    private final JTextField chatField;
-    private final JComboBox draft;
     private WebSocketClient cc;
 
-    public ChatClient(String defaultlocation) {
-        super("WebSocket Chat Client");
-        Container c = getContentPane();
-        GridLayout layout = new GridLayout();
-        layout.setColumns(1);
-        layout.setRows(6);
-        c.setLayout(layout);
+    private static boolean connectStatus = false;
 
-        Draft[] drafts = {new Draft_6455()};
-        draft = new JComboBox(drafts);
-        c.add(draft);
+    private static String message = "{\"uniqueID\":\"EUMM1366149\",\"correlationID\":\"0\",\"workflowRequest\":\"DELETE\"" +
+            ",\"product\":\"MONEYMARKET\",\"makerIndex\":\"1\",\"requestParameters\":[{\"makerIndex\":\"2\",\"parameter\":" +
+            "\"QUOTE_ID\",\"value\":\"2\"}],\"requestValues\":[{\"makerIndex\":\"1\",\"legIndex\":\"1\",\"field\":\"MARKET\"," +
+            "\"subField\":null,\"value\":\"USD\"}]}";
 
-        uriField = new JTextField();
-        uriField.setText(defaultlocation);
-        c.add(uriField);
+    public void actionPerformed() {
 
-        connect = new JButton("Connect");
-        connect.addActionListener(this);
-        c.add(connect);
-
-        close = new JButton("Close");
-        close.addActionListener(this);
-        close.setEnabled(false);
-        c.add(close);
-
-        JScrollPane scroll = new JScrollPane();
-        ta = new JTextArea();
-        scroll.setViewportView(ta);
-        c.add(scroll);
-
-        chatField = new JTextField();
-        chatField.setText("");
-        chatField.addActionListener(this);
-        c.add(chatField);
-
-        java.awt.Dimension d = new java.awt.Dimension(300, 400);
-        setPreferredSize(d);
-        setSize(d);
-
-        addWindowListener(new java.awt.event.WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                if (cc != null) {
-                    cc.close();
-                }
-                dispose();
-            }
-        });
-
-        setLocationRelativeTo(null);
-        setVisible(true);
-    }
-
-    public void actionPerformed(ActionEvent e) {
-
-        if (e.getSource() == chatField) {
-            if (cc != null) {
-                cc.send(chatField.getText());
-                chatField.setText("");
-                chatField.requestFocus();
-            }
-
-        } else if (e.getSource() == connect) {
+        if (connectStatus) {
             try {
                 // cc = new ChatClient(new URI(uriField.getText()), area, ( Draft ) draft.getSelectedItem() );
-                cc = new WebSocketClient(new URI(uriField.getText()), (Draft) draft.getSelectedItem()) {
+                cc = new WebSocketClient(new URI(locationToConnect)) {
 
                     @Override
                     public void onMessage(String message) {
-                        ta.append("got: " + message + "\n");
-                        ta.setCaretPosition(ta.getDocument().getLength());
+                        System.out.println("got: " + message + "\n");
                     }
 
                     @Override
                     public void onOpen(ServerHandshake handshake) {
-                        ta.append("You are connected to ChatServer: " + getURI() + "\n");
-                        ta.setCaretPosition(ta.getDocument().getLength());
+                        System.out.println("You are connected to ChatServer: " + getURI() + "\n");
                     }
 
                     @Override
                     public void onClose(int code, String reason, boolean remote) {
-                        ta.append(
+                        System.out.println(
                                 "You have been disconnected from: " + getURI() + "; Code: " + code + " " + reason
-                                        + "\n");
-                        ta.setCaretPosition(ta.getDocument().getLength());
-                        connect.setEnabled(true);
-                        uriField.setEditable(true);
-                        draft.setEditable(true);
-                        close.setEnabled(false);
+                                        + "\n"
+                        );
                     }
 
                     @Override
                     public void onError(Exception ex) {
-                        ta.append("Exception occurred ...\n" + ex + "\n");
-                        ta.setCaretPosition(ta.getDocument().getLength());
-                        ex.printStackTrace();
-                        connect.setEnabled(true);
-                        uriField.setEditable(true);
-                        draft.setEditable(true);
-                        close.setEnabled(false);
+                        System.out.println("Exception occurred ...\n" + ex + "\n");
                     }
                 };
-
-                close.setEnabled(true);
-                connect.setEnabled(false);
-                uriField.setEditable(false);
-                draft.setEditable(false);
                 cc.connect();
             } catch (URISyntaxException ex) {
-                ta.append(uriField.getText() + " is not a valid WebSocket URI\n");
+                System.out.println(locationToConnect + " is not a valid WebSocket URI\n");
             }
-        } else if (e.getSource() == close) {
+        } else if (connectStatus) {
             cc.close();
+        }
+
+        if (cc != null && message != null) {
+            cc.send(message);
         }
     }
 
@@ -150,10 +71,13 @@ public class ChatClient extends JFrame implements ActionListener {
             location = args[0];
             System.out.println("Default server url specified: \'" + location + "\'");
         } else {
-            location = "ws://localhost:8887";
+            location = locationToConnect;
             System.out.println("Default server url not specified: defaulting to \'" + location + "\'");
         }
-        new ChatClient(location);
+        connectStatus = true;
+        while (true) {
+            new ChatClient().actionPerformed();
+        }
     }
 
 }
